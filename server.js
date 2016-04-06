efb05406f3577aa38c0c5da2ccfa7c756ff53e0c
Natalie Macias <nramirez787@gmail.com>
2016-04-06 12:21:02 -1000

    initializing server connection to client

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
+
+var server = http.createServer().listen(8080, 'localhost');
+  console.log('connected');
+  server.on('request', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html', 'Server-Name': 'NatNat'});
+    console.log('poop');
+    res.end();
+  });
